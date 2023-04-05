6f9120b274c0f958fc92b4a40ba97b77960a4558
Ahmed Izz Murtaja <56529633+ahmedmurtaja@users.noreply.github.com>
2023-04-05 14:45:59 +0200

    Create Server and sql relates #11 Closes #11

--- src/database/config/build.sql
+++ src/database/config/build.sql
@@ -1,17 +1,25 @@
-CREATE TABLE "Employee"(
-    "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR(255) NOT NULL,
-    "position" VARCHAR(255) NOT NULL,
-    "email" VARCHAR(255) NOT NULL
+BEGIN;
+
+DROP TABLE IF EXISTS Tasks CASCADE;
+DROP TABLE IF EXISTS Employee CASCADE;
+
+CREATE TABLE Employee(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    position VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE "Tasks"(
-    "id" BIGINT NOT NULL,
-    "name" VARCHAR(255) NOT NULL,
-    "description" VARCHAR(255) NOT NULL,
-    "employee id" BIGINT NOT NULL
+CREATE TABLE Tasks(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    employee_id INTEGER NOT NULL REFERENCES Employee(id)
 );
-ALTER TABLE
-    "Tasks" ADD PRIMARY KEY("id");
-ALTER TABLE
-    "Tasks" ADD CONSTRAINT "tasks_employee id_foreign" FOREIGN KEY("employee id") REFERENCES "Employee"("id");
+
+INSERT INTO Employee(name, position, email) VALUES ('John', 'Manager', 'john@gmail.com')
+, ('Mike', 'Developer', 'mike@mm.com')
+, ('Sara', 'Developer', 'sssss');
+INSERT INTO Tasks(name, description, employee_id) VALUES ('Task 1', 'Description 1', 1)
+, ('Task 2', 'Description 2', 2);
+COMMIT;
