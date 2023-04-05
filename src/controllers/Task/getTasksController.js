ac95a4a134c39856e8440c09ef62464d807dd141
Mohammed Mahani <mohammed.mahanu@gmail.com>
2023-04-05 22:24:59 +0300

    create-getTasks Query and controller Relates #17 Closes #17

---
+++ src/controllers/Task/getTasksController.js
@@ -0,0 +1,11 @@
+const { getTasksQuery } = require("../../database/queries/getData");
+
+const getTasks = (req, res) => {
+  getTasksQuery()
+    .then((data) => res.json(data.rows))
+    .catch((err) => res.status(500).json({ message: err.message }));
+};
+
+module.exports = {
+  getTasks,
+};
