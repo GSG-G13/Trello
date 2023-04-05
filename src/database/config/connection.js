098e80419ca6c5b54686a0281739c8fb8b9165bd
Mohammed Mahani <mohammed.mahanu@gmail.com>
2023-04-05 15:28:08 +0300

    add-connection-file Relates #13 Closes #13

---
+++ src/database/config/connection.js
@@ -0,0 +1,11 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const connection = new Pool({
+  connectionString: process.env.DB_URL,
+  ssl: false,
+});
+
+module.exports = {
+  connection,
+};
